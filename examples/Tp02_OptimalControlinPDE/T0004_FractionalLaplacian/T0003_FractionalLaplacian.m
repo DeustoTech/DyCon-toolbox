e38a0689bef1225ace4d80e38cf217e23b0c07db
Umberto Biccari <u.biccari@gmail.com>
2019-02-20 09:10:38 +0100

    Auto stash before merge of "master" and "origin/master"

--- examples/Tp02_OptimalControlinPDE/T0004_FractionalLaplacian/T0003_FractionalLaplacian.m
+++ examples/Tp02_OptimalControlinPDE/T0004_FractionalLaplacian/T0003_FractionalLaplacian.m
@@ -1,3 +1,27 @@
+%% 
+% We use DyCon Toolbox for solving numerically the following control 
+% problem: given any $T>0$, find a control function $g\in L^2((−1,1)\times (0,T))$ 
+% such that the corresponding solution to the parabolic problem
+%%
+% \begin{equation*}
+%   \begin{cases}
+%       z_t+(d_x^2)^s z = g\chi_\omega, & (x,t)\in(-1,1)\times(0,T)
+%       z = 0, & (x,t)\in[\mathbb{R}\setminus(-1,1)]\times(0,T)
+%       z(x,0) = z_0(x), & x\in(-1,1)
+%   \end{cases}
+% \end{equation*}
+% satisfies $z(x,T)=0$.
+%%
+% Here, for all $s\in(0,1)$, $(-d_x^2)^s$ denotes the one-dimensional 
+% fractional Laplace operator, defined as the following singular integral
+%%
+% \begin{equation*}
+%   (-d_x^2)^s z(x) = c_s P.V. \int_{\mathbb{R}}
+%   \frac{z(x)-z(y)}{|x-y|^{1+2s}}\,dy.
+% \end{equation*}
+%% 
+% Let us consider a uniform partition of the space interval $(-1,1)$.
+
 %% Parametros de discretizacion
 N = 20;
 xi = -1; xf = 1;
@@ -24,29 +48,20 @@ YT = 0.0*xline';
 
 dx = xline(2) -xline(1);
 
-symPsi  = dx*N*(YT - Y).'*(YT - Y);
-symL    = 0.001*(U.'*U);
+symPsi  = dx*(YT - Y).'*(YT - Y);
+symL    = dx*0.001*(U.'*U);
 iCP1 = OptimalControl(dynamics,symPsi,symL);
 
 %% Solve Gradient
-tol = 0.01;
-
-%
-GradientMethod(iCP1,'graphs',true,'TypeGraphs','PDE','MaxIter',200,'DescentAlgorithm',@ConjugateGradientDescent)
-GradientMethod(iCP1,'graphs',true,'TypeGraphs','PDE','MaxIter',200,'DescentAlgorithm',@ClassicalDescent)
-
+tol = 0.00001;
 %%
-solve(dynamics)
+GradientMethod(iCP1,'DescentAlgorithm',@AdaptativeDescent,'tol',tol)
+plot(iCP1,'TypeGraphs','PDE')
 
-iCP1.ode.label = 'Control';
-dynamics.label = 'Dynamics';
+% iCP1.ode.label = 'Control';
+% dynamics.label = 'Dynamics';
 % animation([iCP1.ode,dynamics],'YLim',[-1 1],'xx',0.05)
-% Several ways to run
-% GradientMethod(iCP1)
-% GradientMethod(iCP1,'DescentParameters',DescentParameters)
-% GradientMethod(iCP1,'DescentParameters',DescentParameters,'graphs',true)
 
-% iCP1.ode
 function [B] = construction_matrix_B(mesh,a,b)
 
 N = length(mesh);
