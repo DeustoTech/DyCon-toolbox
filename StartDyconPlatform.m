6679879711ccfef7292caed2722390cae9953cf6
azaharmonge <azahar.sz@gmail.com>
2019-03-06 12:45:27 +0100

    Update StartDyconPlatform.m

--- StartDyconPlatform.m
+++ StartDyconPlatform.m
@@ -1,6 +1,6 @@
 function StartDyconPlatform()
 %STARTDYCONPLATFORM - Agrea las carpetas necesarias para ejecutar todos los tutoriales
-file        = 'StartDyconPlatform.m';w
+file        = 'StartDyconPlatform.m';
 pathfile    =  replace(which(file),file,''); 
 addpath(genpath(pathfile))
 end
