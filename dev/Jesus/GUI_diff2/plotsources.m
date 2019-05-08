384e8b3790a4e915520dc2174992200ac5d9a7a8
Jesus Oroya <djoroya@deusto.es>
2019-05-08 13:59:48 +0200

    Remove diff2

--- dev/Jesus/GUI_diff2/plotsources.m
+++
@@ -1,9 +0,0 @@
-function plotsources(axes,Sources)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-line([Sources.x0],[Sources.y0],'Marker','.','LineStyle','none','MarkerSize',8,'Color','k','Parent',axes)
-line([Sources.x0],[Sources.y0],'Marker','o','LineStyle','none','MarkerSize',10,'Color','k','Parent',axes)
-
-end
-
